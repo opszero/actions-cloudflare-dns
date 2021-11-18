965c481ea96ae82b498a3eba9e6d14709841d837
Abhi Yerra <abhi@berkeley.edu>
2021-11-18 15:19:55 -0800

    Fix

--- main.js
+++ main.js
@@ -31,9 +31,6 @@ const getCurrentRecordId = () => {
   const name = core.getInput('name');
   const record = result.find((x) => x.name == `${name}.${x.zone_name}`);
 
-  console.log(result)
-  console.log(result[0].id)
-
   if (!record) {
     return null
   }
