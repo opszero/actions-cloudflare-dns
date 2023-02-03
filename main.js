160ba8d35f80b3885d4c653ea1ed051c84c8b26d
Abhi Yerra <abhi@berkeley.edu>
2023-02-03 12:56:16 -0800

    Test

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ const getCurrentRecordId = () => {
   const name = core.getInput('name');
   const record = result.find((x) => x.name == `${name}.${x.zone_name}`);
 
-  console.log("Lo", record)
+  console.log("Lo", name, record)
 
   if (!record) {
     return null
